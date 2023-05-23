28f5aaee17b8497f551b7b70577a1241af3be768
Jeroen Bos <jer123bos@gmail.com>
2023-05-23 11:53:44 +0200

    Copy over some lodash parts

---
+++ src/lodash.ts
@@ -0,0 +1,219 @@
+// I don't want to depend on a dependency, so just copying the parts of lodash that I want. Unfortunately it's quite the hierarchy
+// Copied from https://github.com/lodash/lodash/tree/2da024c3b4f9947a48517639de7560457cd4ec6c
+
+export function set(object, path, value) {
+  return object == null ? object : baseSet(object, path, value)
+}
+export function get(object, path, defaultValue) {
+  const result = object == null ? undefined : baseGet(object, path)
+  return result === undefined ? defaultValue : result
+}
+const hasOwnProperty = Object.prototype.hasOwnProperty
+
+function baseSet(object, path, value, customizer = undefined) {
+  if (!isObject(object)) {
+    return object
+  }
+  path = castPath(path, object)
+
+  const length = path.length
+  const lastIndex = length - 1
+
+  let index = -1
+  let nested = object
+
+  while (nested != null && ++index < length) {
+    const key = toKey(path[index])
+    let newValue = value
+
+    if (index != lastIndex) {
+      const objValue = nested[key]
+      newValue = customizer ? customizer(objValue, key, nested) : undefined
+      if (newValue === undefined) {
+        newValue = isObject(objValue)
+          ? objValue
+          : (isIndex(path[index + 1]) ? [] : {})
+      }
+    }
+    assignValue(nested, key, newValue)
+    nested = nested[key]
+  }
+  return object
+}
+
+function assignValue(object, key, value) {
+  const objValue = object[key]
+
+  if (!(hasOwnProperty.call(object, key) && eq(objValue, value))) {
+    if (value !== 0 || (1 / value) === (1 / objValue)) {
+      baseAssignValue(object, key, value)
+    }
+  } else if (value === undefined && !(key in object)) {
+    baseAssignValue(object, key, value)
+  }
+}
+function baseAssignValue(object, key, value) {
+  if (key == '__proto__') {
+    Object.defineProperty(object, key, {
+      'configurable': true,
+      'enumerable': true,
+      'value': value,
+      'writable': true
+    })
+  } else {
+    object[key] = value
+  }
+}
+function eq(value, other) {
+  return value === other || (value !== value && other !== other)
+}
+const charCodeOfDot = '.'.charCodeAt(0)
+const reEscapeChar = /\\(\\)?/g
+const rePropName = RegExp(
+  // Match anything that isn't a dot or bracket.
+  '[^.[\\]]+' + '|' +
+  // Or match property names within brackets.
+  '\\[(?:' +
+    // Match a non-string expression.
+    '([^"\'][^[]*)' + '|' +
+    // Or match strings (supports escaping characters).
+    '(["\'])((?:(?!\\2)[^\\\\]|\\\\.)*?)\\2' +
+  ')\\]'+ '|' +
+  // Or match "" as the space between consecutive dots or empty brackets.
+  '(?=(?:\\.|\\[\\])(?:\\.|\\[\\]|$))'
+  , 'g')
+  const MAX_MEMOIZE_SIZE = 500
+  function memoizeCapped(func) {
+    const result = memoize(func, (key) => {
+      const { cache } = result
+      if (cache.size === MAX_MEMOIZE_SIZE) {
+        cache.clear()
+      }
+      return key
+    })
+  
+    return result
+  }
+  function memoize(func, resolver) {
+    if (typeof func !== 'function' || (resolver != null && typeof resolver !== 'function')) {
+      throw new TypeError('Expected a function')
+    }
+    const memoized = function(...args) {
+      const key = resolver ? resolver.apply(this, args) : args[0]
+      const cache = memoized.cache
+  
+      if (cache.has(key)) {
+        return cache.get(key)
+      }
+      const result = func.apply(this, args)
+      memoized.cache = cache.set(key, result) || cache
+      return result
+    }
+    memoized.cache = new (memoize.Cache || Map)
+    return memoized
+  }
+  
+memoize.Cache = Map
+const stringToPath = memoizeCapped((string) => {
+  const result = []
+  if (string.charCodeAt(0) === charCodeOfDot) {
+    result.push('')
+  }
+  string.replace(rePropName, (match, expression, quote, subString) => {
+    let key = match
+    if (quote) {
+      key = subString.replace(reEscapeChar, '$1')
+    }
+    else if (expression) {
+      key = expression.trim()
+    }
+    result.push(key)
+  })
+  return result
+})
+function castPath(value, object) {
+  if (Array.isArray(value)) {
+    return value
+  }
+  return isKey(value, object) ? [value] : stringToPath(value)
+}
+const reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/
+const reIsPlainProp = /^\w*$/
+function isKey(value, object) {
+  if (Array.isArray(value)) {
+    return false
+  }
+  const type = typeof value
+  if (type === 'number' || type === 'boolean' || value == null || isSymbol(value)) {
+    return true
+  }
+  return reIsPlainProp.test(value) || !reIsDeepProp.test(value) ||
+    (object != null && value in Object(object))
+}
+function isSymbol(value) {
+  const type = typeof value
+  return type == 'symbol' || (type === 'object' && value != null && getTag(value) == '[object Symbol]')
+}
+const toString = Object.prototype.toString
+
+/**
+ * Gets the `toStringTag` of `value`.
+ *
+ * @private
+ * @param {*} value The value to query.
+ * @returns {string} Returns the `toStringTag`.
+ */
+function getTag(value) {
+  if (value == null) {
+    return value === undefined ? '[object Undefined]' : '[object Null]'
+  }
+  return toString.call(value)
+}
+
+/** Used as references for various `Number` constants. */
+const MAX_SAFE_INTEGER = 9007199254740991
+
+/** Used to detect unsigned integer values. */
+const reIsUint = /^(?:0|[1-9]\d*)$/
+
+/**
+ * Checks if `value` is a valid array-like index.
+ *
+ * @private
+ * @param {*} value The value to check.
+ * @param {number} [length=MAX_SAFE_INTEGER] The upper bounds of a valid index.
+ * @returns {boolean} Returns `true` if `value` is a valid index, else `false`.
+ */
+function isIndex(value, length = undefined) {
+  const type = typeof value
+  length = length == null ? MAX_SAFE_INTEGER : length
+
+  return !!length &&
+    (type === 'number' ||
+      (type !== 'symbol' && reIsUint.test(value))) &&
+        (value > -1 && value % 1 == 0 && value < length)
+}
+function isObject(value) {
+  const type = typeof value
+  return value != null && (type === 'object' || type === 'function')
+}
+const INFINITY = 1 / 0
+function toKey(value) {
+  if (typeof value === 'string' || isSymbol(value)) {
+    return value
+  }
+  const result = `${value}`
+  return (result == '0' && (1 / value) == -INFINITY) ? '-0' : result
+}
+
+function baseGet(object, path) {
+  path = castPath(path, object)
+
+  let index = 0
+  const length = path.length
+
+  while (object != null && index < length) {
+    object = object[toKey(path[index++])]
+  }
+  return (index && index == length) ? object : undefined
+}
